cf79d7c0c482216ca7ddd9dd65658c83208181eb
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-07-17 13:33:54 +0700

    Added BizzySalesOrder initializer

--- api/SOInjector.java
+++ api/SOInjector.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -25,7 +26,6 @@ import org.compiere.util.CLogger;
 import org.compiere.util.Env;
 
 import org.supercsv.cellprocessor.Optional;
-import org.supercsv.cellprocessor.constraint.NotNull;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvMapWriter;
 import org.supercsv.io.ICsvMapWriter;
@@ -42,6 +42,35 @@ public class SOInjector {
     public static final String PLUGIN_PREFIX = "[SAS SO Injector] ";
 
     protected static CLogger log = CLogger.getCLogger(SOInjector.class);
+
+    public static boolean dummyTrigger() {
+        BizzySalesOrder bizzySo = new BizzySalesOrder();
+
+        bizzySo.soff_code = 'A';
+        bizzySo.description = "OSGi Testing v1";
+        bizzySo.dateOrdered = new Date(System.currentTimeMillis());
+        bizzySo.bpHoldingNo = 3806;
+        bizzySo.bpLocationName = "PIONEER ELECTRIC- Kenari Mas [Kenari Mas Jl. Kramat Raya Lt. Dasar Blok C No. 3-5]";
+
+        bizzySo.orderLines = new BizzySalesOrderLine[5];
+        bizzySo.orderLines[0] = new BizzySalesOrderLine();
+        bizzySo.orderLines[0].productId = "AB0301485";
+        bizzySo.orderLines[0].quantity = 20;
+        bizzySo.orderLines[1] = new BizzySalesOrderLine();
+        bizzySo.orderLines[1].productId = "AB0301440";
+        bizzySo.orderLines[1].quantity = 40;
+        bizzySo.orderLines[2] = new BizzySalesOrderLine();
+        bizzySo.orderLines[2].productId = "AB0301430";
+        bizzySo.orderLines[2].quantity = 60;
+        bizzySo.orderLines[3] = new BizzySalesOrderLine();
+        bizzySo.orderLines[3].productId = "AB0301420";
+        bizzySo.orderLines[3].quantity = 80;
+        bizzySo.orderLines[4] = new BizzySalesOrderLine();
+        bizzySo.orderLines[4].productId = "AB0301635";
+        bizzySo.orderLines[4].quantity = 100;
+
+        return apiName(bizzySo);
+    }
 
     public static boolean apiName(BizzySalesOrder bizzySo) {
         SASSalesOrder sasSo = new SASSalesOrder(bizzySo);
