12ba01366f157e47478b213176586b539822edf7
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-07-21 09:09:46 +0700

    Working-locally rmi client

--- rmi/Client.java
+++ rmi/Client.java
@@ -1,17 +1,17 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.Date;
 
 public class Client {
     private Client() {}
 
     public static void main(String[] args) {
-        String host = (args.length < 1) ? null : args[0]; // agrs[0] == host
+        String host = (args.length < 1) ? null : args[0]; 
+        int port = (args.length < 1) ? null : Integer.parseInt(args[1]); 
 
         try {
-            Registry registry = LocateRegistry.getRegistry(host);
-            RemoteApi stub = (RemoteApi) registry.lookup("RemoteApi3");
-            String response = stub.testingRmi();
+            Registry registry = LocateRegistry.getRegistry(host, port);
+            RemoteApi stub = (RemoteApi) registry.lookup("RemoteApi");
+            boolean response = stub.injectSo(null); // TODO
             System.out.println("response: " + response);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
