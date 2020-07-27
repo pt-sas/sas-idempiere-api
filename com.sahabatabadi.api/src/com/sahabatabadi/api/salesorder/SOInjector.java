0873eb222cc00da20cc095d49533f75a45c4a2db
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-07-27 14:13:09 +0700

    Removed createBizzyTestSO

--- com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/SOInjector.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/SOInjector.java
@@ -245,33 +245,4 @@ public class SOInjector {
             }
         }
     }
-
-    public static BizzySalesOrder createTestBizzySo() {
-        BizzySalesOrder bizzySo = new BizzySalesOrder();
-
-        bizzySo.soff_code = 'A';
-        bizzySo.description = "OSGi Testing v2";
-        bizzySo.dateOrdered = new Date(System.currentTimeMillis());
-        bizzySo.bpHoldingNo = 3806;
-        bizzySo.bpLocationName = "PIONEER ELECTRIC- Kenari Mas [Kenari Mas Jl. Kramat Raya Lt. Dasar Blok C No. 3-5]";
-
-        bizzySo.orderLines = new BizzySalesOrderLine[5];
-        bizzySo.orderLines[0] = new BizzySalesOrderLine();
-        bizzySo.orderLines[0].productId = "AB0301485";
-        bizzySo.orderLines[0].quantity = 20;
-        bizzySo.orderLines[1] = new BizzySalesOrderLine();
-        bizzySo.orderLines[1].productId = "AB0301440";
-        bizzySo.orderLines[1].quantity = 30;
-        bizzySo.orderLines[2] = new BizzySalesOrderLine();
-        bizzySo.orderLines[2].productId = "AB0301430";
-        bizzySo.orderLines[2].quantity = 40;
-        bizzySo.orderLines[3] = new BizzySalesOrderLine();
-        bizzySo.orderLines[3].productId = "AB0301420";
-        bizzySo.orderLines[3].quantity = 50;
-        bizzySo.orderLines[4] = new BizzySalesOrderLine();
-        bizzySo.orderLines[4].productId = "AB0301635";
-        bizzySo.orderLines[4].quantity = 60;
-
-        return bizzySo;
-    }
 }
