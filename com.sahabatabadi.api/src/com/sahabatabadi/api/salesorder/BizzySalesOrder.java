e50dbf8312bb5dfd785f152039ed25cf9703a2ba
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-07-28 15:11:09 +0700

    Added empty constructor and copy constructor

--- com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrder.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrder.java
@@ -15,4 +15,24 @@ public class BizzySalesOrder implements Serializable {
     public char orderSource;
 
     public BizzySalesOrderLine[] orderLines;
+
+    /**
+     * Empty constructor
+     */
+    public BizzySalesOrder() {
+        bpHoldingNo = -1;
+    }
+
+    /**
+     * Copy constructor
+     */
+    public BizzySalesOrder(BizzySalesOrder oldBizzySo) {
+        this.soff_code      = oldBizzySo.soff_code;
+        this.description    = oldBizzySo.description;
+        this.dateOrdered    = oldBizzySo.dateOrdered;
+        this.bpHoldingNo    = oldBizzySo.bpHoldingNo;
+        this.bpLocationName = oldBizzySo.bpLocationName;
+        this.orderSource    = oldBizzySo.orderSource;
+        this.orderLines     = oldBizzySo.orderLines;
+    }
 }
--- com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrderLine.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrderLine.java
@@ -10,4 +10,22 @@ public class BizzySalesOrderLine implements Serializable {
 
     public char principalId;
     public int discount;
+
+    /**
+     * Empty constructor
+     */
+    public BizzySalesOrderLine() {
+        this.quantity = -1;
+        this.discount = -1;
+    } 
+
+    /**
+     * Copy constructor
+     */
+    public BizzySalesOrderLine(BizzySalesOrderLine bizzySoLine) {
+        this.productId = bizzySoLine.productId;
+        this.quantity = bizzySoLine.quantity;
+        this.principalId = bizzySoLine.principalId;
+        this.discount = bizzySoLine.discount;
+    }
 }
