94294ae9c0fc84d92a7a59cedc1cc03daf55eee1
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-08-04 17:41:49 +0700

    Refactored emulateLogin into Activator

--- com.sahabatabadi.api/src/com/sahabatabadi/api/Activator.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/Activator.java
@@ -29,11 +29,44 @@ public class Activator implements BundleActivator {
             log.info("SAS iDempiere API is starting");
         Activator.context = bundleContext;
 
+        emulateLogin();
+
         if (rmiServer == null) {
             rmiServer = new RMIServer();
         }
 
         rmiServer.startRmiServer();
+    }
+
+    private static void emulateLogin() {
+        // TODO emulate proper login
+        Env.setContext(Env.getCtx(), "#AD_User_Name", "Fajar-170203");
+        Env.setContext(Env.getCtx(), "#AD_User_ID", 2211127);
+        Env.setContext(Env.getCtx(), "#SalesRep_ID", 2211127);
+        Env.setContext(Env.getCtx(), "#AD_Role_ID", 1000110);
+        Env.setContext(Env.getCtx(), "#AD_Role_Name", "Role SLS Admin");
+        // Env.setContext(Env.getCtx(), "#AD_User_Name", "Api-01");
+        // Env.setContext(Env.getCtx(), "#AD_User_ID", 2214213);
+        // Env.setContext(Env.getCtx(), "#SalesRep_ID", 2214213);
+        // Env.setContext(Env.getCtx(), "#AD_Role_ID", 1000002);
+        // Env.setContext(Env.getCtx(), "#AD_Role_Name", "awn Admin");
+        Env.setContext(Env.getCtx(), "#User_Level", " CO"); // Format 'SCO'
+        Env.setContext(Env.getCtx(), Env.AD_ORG_ID, 1000001);
+        Env.setContext(Env.getCtx(), Env.AD_ORG_NAME, "Sunter");
+        Env.setContext(Env.getCtx(), Env.M_WAREHOUSE_ID, 1000000);
+        Env.setContext(Env.getCtx(), "#Date", new Timestamp(System.currentTimeMillis()));
+        Env.setContext(Env.getCtx(), "#ShowAcct", "N");
+        Env.setContext(Env.getCtx(), "#ShowTrl", "Y");
+        Env.setContext(Env.getCtx(), "#ShowAdvanced", "N");
+        Env.setContext(Env.getCtx(), "#YYYY", "Y");
+        Env.setContext(Env.getCtx(), "#StdPrecision", 2);
+        Env.setContext(Env.getCtx(), "$C_AcctSchema_ID", 1000001);
+        Env.setContext(Env.getCtx(), "$C_Currency_ID", 303);
+        Env.setContext(Env.getCtx(), "$HasAlias", "Y");
+        Env.setContext(Env.getCtx(), "#C_Country_ID", 100);
+        Env.setContext(Env.getCtx(), Env.LANGUAGE, "en_US");
+        Env.setContext(Env.getCtx(), "#AD_Client_ID", 1000001);
+        Env.setContext(Env.getCtx(), "#AD_Client_Name", "sas");
     }
 
     /*
