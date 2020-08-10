22c0941e28452af4a3a6503132bd6f4d6d07dbc0
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-08-10 15:07:16 +0700

    Added Javadoc comments for BizzySO objects

--- com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrder.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrder.java
@@ -3,17 +3,54 @@ package com.sahabatabadi.api.salesorder;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * POJO class to represent Sales Order Header data from Bizzy.
+ * 
+ * @author Nicholas Alexander Limit
+ * @version 1.0
+ */
 public class BizzySalesOrder implements Serializable {
-	private static final long serialVersionUID = 20200728L;
+    private static final long serialVersionUID = 20200728L;
 
+    /**
+     * Represents the branch associated with the sales order. Value has to be either
+     * {@code 'A'}, {@code 'B'}, {@code 'C'}, {@code 'D'}, or {@code 'M'}.
+     */
     public char soff_code;
+
+    /**
+     * Raw description of the sales order. Can be left empty.
+     */
     public String description;
+
+    /**
+     * Date the sales order was created.
+     */
     public Date dateOrdered;
+
+    /**
+     * Five-digit business partner (BP) or customer number, as an int. For example,
+     * for BP "PIONEER ELEKTRIC" with BP number "03806", the value is {@code 3806}.
+     */
     public int bpHoldingNo;
+
+    /**
+     * Exact name of the business partner's (BP) or customer's location. Value has
+     * to exactly match the name in iDempiere's {@code C_BPartner_Location} table.
+     * Example: {@code "PIONIR ELEKTRIK INDONESIA [Kenari Mas Jl. Kramat Raya Lt.
+     * Dasar Blok C No. 3-5]"}
+     */
     public String bpLocationName;
 
+    /**
+     * Bizzy source of this order. Value has to be either {@code 'B'} (representing
+     * BFF), or {@code 'S'} (representing TokoSmart).
+     */
     public char orderSource;
 
+    /**
+     * Array of {@link BizzySalesOrderLine} object associated with this header.
+     */
     public BizzySalesOrderLine[] orderLines;
 
     /**
@@ -25,14 +62,16 @@ public class BizzySalesOrder implements Serializable {
 
     /**
      * Copy constructor
+     * 
+     * @param bizzySo Bizzy SO object to copy from
      */
-    public BizzySalesOrder(BizzySalesOrder oldBizzySo) {
-        this.soff_code      = oldBizzySo.soff_code;
-        this.description    = oldBizzySo.description;
-        this.dateOrdered    = oldBizzySo.dateOrdered;
-        this.bpHoldingNo    = oldBizzySo.bpHoldingNo;
-        this.bpLocationName = oldBizzySo.bpLocationName;
-        this.orderSource    = oldBizzySo.orderSource;
-        this.orderLines     = oldBizzySo.orderLines;
+    public BizzySalesOrder(BizzySalesOrder bizzySo) {
+        this.soff_code = bizzySo.soff_code;
+        this.description = bizzySo.description;
+        this.dateOrdered = bizzySo.dateOrdered;
+        this.bpHoldingNo = bizzySo.bpHoldingNo;
+        this.bpLocationName = bizzySo.bpLocationName;
+        this.orderSource = bizzySo.orderSource;
+        this.orderLines = bizzySo.orderLines;
     }
 }
--- com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrderLine.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/salesorder/BizzySalesOrderLine.java
@@ -2,13 +2,38 @@ package com.sahabatabadi.api.salesorder;
 
 import java.io.Serializable;
 
+/**
+ * POJO class to represent Sales Order Line data from Bizzy. Used in
+ * {@link BizzySalesOrder}.
+ * 
+ * @author Nicholas Alexander Limit
+ * @version 1.0
+ */
 public class BizzySalesOrderLine implements Serializable {
-	private static final long serialVersionUID = 20200730L;
+    private static final long serialVersionUID = 20200730L;
 
+    /**
+     * Product ID of the product being ordered. Has to match the entries in
+     * iDempiere's {@code M_Product} table. Example: {@code "AB0301485"}.
+     */
     public String productId;
+
+    /**
+     * Quantity of the product being ordered.
+     */
     public int quantity;
 
+    /**
+     * Principal or brand of the product being ordered. Can be left empty. If
+     * filled, has to match the entries in iDempiere's {@code M_Product} table.
+     * Example: {@code "Philips"}.
+     */
     public String principalId;
+
+    /**
+     * Discount amount in percent of the product being ordered. Can be left empty.
+     * Example: {@code 7.51} (%).
+     */
     public double discount;
 
     /**
@@ -17,10 +42,12 @@ public class BizzySalesOrderLine implements Serializable {
     public BizzySalesOrderLine() {
         this.quantity = -1;
         this.discount = -1;
-    } 
+    }
 
     /**
      * Copy constructor
+     * 
+     * @param bizzySoLine Bizzy SO Line object to copy from
      */
     public BizzySalesOrderLine(BizzySalesOrderLine bizzySoLine) {
         this.productId = bizzySoLine.productId;
