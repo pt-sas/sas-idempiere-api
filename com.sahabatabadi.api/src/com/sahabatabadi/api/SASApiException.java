7c856068a103bf833d1fe0d3d0d5a4bf50c10dfa
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2020-08-27 17:43:44 +0700

    Added Javadoc comments

--- com.sahabatabadi.api/src/com/sahabatabadi/api/SASApiException.java
+++ com.sahabatabadi.api/src/com/sahabatabadi/api/SASApiException.java
@@ -1,28 +1,64 @@
 package com.sahabatabadi.api;
 
+/**
+ * Custom Exception class to represent Exceptions being thrown when processing
+ * a {@link Document} object.
+ */
 public class SASApiException extends Exception {
     /**
      * Auto-genereated serial version UID by Eclipse
      */
     private static final long serialVersionUID = 7446213057086374281L;
 
+    /**
+     * Document being processed when exception is thrown
+     */
     private Document doc;
 
+    /**
+     * Constructs a new exception with null as its detail message. The cause is not
+     * initialized, and may subsequently be initialized by a call to initCause.
+     * 
+     * @param currentDoc document being processed when exception is thrown
+     */
     public SASApiException(Document currentDoc) {
         super();
         this.doc = currentDoc;
     }
 
+    /**
+     * Constructs a new exception with the specified detail message. The cause is
+     * not initialized, and may subsequently be initialized by a call to initCause.
+     * 
+     * @param currentDoc document being processed when exception is thrown
+     * @param message    the detail message. The detail message is saved for later
+     *                   retrieval by the getMessage() method.
+     */
     public SASApiException(Document currentDoc, String message) {
         super(message);
         this.doc = currentDoc;
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     * 
+     * @param currentDoc document being processed when exception is thrown
+     * @param message    the detail message (which is saved for later retrieval by
+     *                   the getMessage() method).
+     * @param cause      the cause (which is saved for later retrieval by the
+     *                   getCause() method). (A null value is permitted, and
+     *                   indicates that the cause is nonexistent or unknown.)
+     */
     public SASApiException(Document currentDoc, String message, Throwable cause) {
         super(message, cause);
         this.doc = currentDoc;
     }
 
+    /**
+     * Gets the document being processed when exception is thrown
+     * 
+     * @return document being processed on exception
+     */
     public Document getBadDocument() {
         return this.doc;
     }
